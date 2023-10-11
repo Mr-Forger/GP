732f4531d49accb392301026e9d2b1d79703386b
SeungHan Moon <msh9911@naver.com>
2023-10-11 18:05:15 +0900

    1

--- 231011/응용11_4_1기능추가2.c
+++ 231011/응용11_4_1기능추가2.c
@@ -29,17 +29,18 @@ int main(void)
     system("cls");
     system("clear");
 
-    while (1) 
+    while (1)
     {
         printf("길이에 대한 도량형 환산 프로그램\n\n");
         print_distance_unit();
         printf("\n\n기본단위와 변환단위\n입력 후 Enter(예:1 5)>");
-        
+
         int basic, transe;
         double measure;
         if (scanf("%d %d", &basic, &transe) != 2 || basic < 0 || basic > 9 || transe < 0 || transe > 9)
         {
             printf("잘못된 입력입니다. 다시 입력해주세요.\n");
+            printf("\n");
             continue;
         }
 
@@ -51,12 +52,12 @@ int main(void)
         char again;
         printf("\n계속 하실꺼면 'y', 그만 하실꺼면 'n'을 입력해주세요: ");
         scanf(" %c", &again);
-        
-        if (again == 'n' || again == 'N') 
+
+        if (again == 'n' || again == 'N')
         {
             break; // 프로그램 종료
-        } 
-        else if (again == 'y' || again == 'Y') 
+        }
+        else if (again == 'y' || again == 'Y')
         {
             system("cls");
             system("clear");
@@ -81,15 +82,15 @@ void measure_calculate(int basic, int transe, double measure)
 {
     double msre[10], unit, result;
 
-    msre[0]=392772;		
+    msre[0]=392772;
     msre[1]=msre[0]/100.;
     msre[2]=msre[1]/1000.;
     msre[3]=msre[0]/2.54;
     msre[4]=msre[3]/12.;
     msre[5]=msre[4]/3.;
     msre[6]=msre[5]/1760.;
-    msre[7]=12960.;      	
-    msre[8]=msre[7]/6.;   	
+    msre[7]=12960.;
+    msre[8]=msre[7]/6.;
     msre[9]=msre[8]/(6.*36);
 
     unit = msre[basic];
